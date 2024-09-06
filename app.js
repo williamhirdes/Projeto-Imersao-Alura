302823125685a364df26f5a7148e17acd2c3610d
williamhirdes <42836956+williamhirdes@users.noreply.github.com>
2024-09-06 19:01:28 -0300

    pesquisa

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 function pesquisar() {
+  
   // Obtém a seção onde os resultados da pesquisa serão exibidos
   let section = document.getElementById("resultados-pesquisa");
     
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if(!campoPesquisa){
+    section.innerHTML = "<p>Nenhum dado foi encontrado, digite algo.</p>"
+    return
+}
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
   // Inicializa uma string vazia para armazenar os resultados da pesquisa
   let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
 
   // Itera sobre cada dado na array 'dados'
   for (let dado of dados) {
+
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+
+    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) ){
+
     // Constrói o HTML para cada item do resultado da pesquisa
-    resultados += `<div class="item-resultado">
+      resultados += `<div class="item-resultado">
       <h2>
         <a href="${dado.link}" target="_blank">${dado.titulo}</a>
       </h2>
       <p class="descricao-meta">${dado.descricao}</p>
       <a href="${dado.link}" target="_blank">Mais informações sobre o filme</a>
       </div>`;
+
+
+    }
+    
+    
+  }
+
+  if (!resultados){
+      resultados = "<p>Nenhum dado foi encontrado</p>"
   }
 
   // Atribui o HTML gerado à seção de resultados
--- dados.js
+++ dados.js
@@ -2,36 +2,43 @@ let dados = [
     {
         titulo : "O Poderoso Chefão",
         descricao : "Clássico do cinema dirigido por Francis Ford Coppola, que narra a história da poderosa família mafiosa Corleone. A trama aborda lealdade, poder e vingança, sendo um dos filmes mais influentes de todos os tempos.",
-        link : "https://www.imdb.com/title/tt0068646/"
+        link : "https://www.imdb.com/title/tt0068646/",
+        tags : "Crime, Drama",
     },
     {
         titulo: "Matrix",
         descricao: "Neo descobre que o mundo que conhece é uma simulação de computador criada por máquinas. Com a ajuda de Morpheus e Trinity, ele luta para libertar a humanidade dessa ilusão. Um marco no cinema de ficção científica.",
-        link: "https://www.imdb.com/title/tt0133093/"
+        link: "https://www.imdb.com/title/tt0133093/",
+        tags: "Ficção Científica, Ação",
     },
     {
         titulo: "Clube da Luta",
         descricao: "Um homem comum, insatisfeito com sua vida, encontra um vendedor de sabão carismático e juntos fundam um clube de luta clandestino. Um filme que explora questões sobre a identidade e a sociedade moderna.",
-        link: "https://www.imdb.com/title/tt0137523/"
+        link: "https://www.imdb.com/title/tt0137523/",
+        tags: "Drama, Suspense",
     },
     {
         titulo: "Interestelar",
         descricao: "Num futuro distópico, um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade. Dirigido por Christopher Nolan, o filme combina ciência e emoção de forma única.",
-        link: "https://www.imdb.com/title/tt0816692/"
+        link: "https://www.imdb.com/title/tt0816692/",
+        tags: "Ficção Científica, Aventura, Drama",
     },
     {
         titulo: "Coringa",
         descricao: "A história de origem de um dos vilões mais icônicos da cultura pop. O filme acompanha a transformação de Arthur Fleck, um comediante fracassado, no temido Coringa, em uma Gotham caótica e corrupta.",
-        link: "https://www.imdb.com/title/tt7286456/"
+        link: "https://www.imdb.com/title/tt7286456/",
+        tags: "Drama, Suspense, Crime",
     },
     {
         titulo: "O Senhor dos Anéis: A Sociedade do Anel",
         descricao: "Um hobbit encontra um anel mágico que pode controlar o mundo e inicia uma jornada épica para destruí-lo.",
-        link: "https://www.imdb.com/title/tt0120737/?"
+        link: "https://www.imdb.com/title/tt0120737/",
+        tags: "Fantasia, Aventura, Drama",
     },
     {
         titulo: "Parasita",
         descricao: "Duas famílias de classes sociais opostas se cruzam de forma inesperada, desencadeando uma série de eventos trágicos.",
-        link: "https://www.imdb.com/title/tt6751668/"
-    },
+        link: "https://www.imdb.com/title/tt6751668/",
+        tags: "Drama, Comédia, Suspense",
+    }
 ];
